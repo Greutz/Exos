514035ac8fe80f2811aa353860e9464939c76481
Greutz <gorton83@gmail.com>
2019-10-01 15:57:22 +0200

    exo 21 modified

--- exo_21.rb
+++ exo_21.rb
@@ -7,8 +7,7 @@ else
     1.upto (input) do |i| #On part de 1 jusqu'a l'input de l'utilisateur, on créé un index
 
         input.times {print " "} #A chaque boucle on part de la valeur actuelle de l'index pour écrire le nombre d'espaces avec un print pour ne pas aller à la ligne
-        i.times {print "#"} # A chaque boucle on utilise la valeur actuelle de l'index pour écrire le nombre de #  
-        i += 1 # A chaque fin de boucle on incrémente l'index
+        i.times {print "#"} # A chaque boucle on utilise la valeur actuelle de l'index pour écrire le nombre de # 
         input -= 1 # A chaque fin de boucle on soustrait 1 a la valeur entrée par l'utilisateur 
         puts #saut à la ligne et on recommence
     end
